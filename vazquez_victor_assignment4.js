4ee4e00a1ce7758f3f7071fab642e7ddc1b58380
victorvazco <v_vazquezcobian@yahoo.com>
2014-08-27 03:46:52 -0400

    Check email address Function verified

--- vazquez_victor_assignment4.js
+++ vazquez_victor_assignment4.js
@@ -10,7 +10,8 @@
 
 var theNumber = "100"
 var theDigits = "787-309-3194";
-
+var theEmail = "victorvazco@fullsail.com";
+var theArrayMath = ["4","78",89,45,"What Up!"];
 
 
 //Enter a String and return a Number
@@ -18,7 +19,7 @@ var theDigits = "787-309-3194";
 var stringToNumber = function(number){
     
     
-    var changeO = parseInt(number) + 5;
+    var changeO = parseInt(number) + 5; // math to confirm that string has been changed number that can be worked with... this because in my browser I do not notice any color difference in the console
     
     console.log(changeO);
        
@@ -28,6 +29,9 @@ var stringToNumber = function(number){
     return changeO;
     
 };
+
+
+
 
 
 //Verify if string entered is a telephone number in the right format Function
@@ -51,6 +55,49 @@ var telephoneChecker = function(telNumber){
 
 
 
+
+//Verify if email entered is correct
+
+var checkEmail = function(email){
+    
+    var stop = email.length;
+    
+    var beginEnd = email.lastIndexOf(".")+1;
+    
+
+    
+    if (email.substring(0,stop) === "@" || email.substring(beginEnd,stop) === "com"){
+        
+        var yesCorrect = true;
+        return yesCorrect;
+    
+      } else {
+        
+        var nonCorrect = false;  
+    
+    }
+        return nonCorrect;
+
+};
+
+
+
+
+
+//Add only the number characters in an array that includes strings
+
+var addArray = function(arrayInfo){
+    
+    var theMath = arrayInfo[0] + arrayInfo[1] + arrayInfo[2] + arrayInfo[3] + arrayInfo[4];
+    
+    return theMath;
+    
+};
+
+
+
+
+
 //Main Code/Callers
 
 var outComesNumber = stringToNumber(theNumber);
@@ -58,3 +105,9 @@ console.log("The number is " + outComesNumber);
 
 var verifyTel = telephoneChecker(theDigits);
 console.log("The telephone number is " + verifyTel);
+
+var emailChecker = checkEmail(theEmail);
+console.log("This is a " + emailChecker + " email address.");
+
+var theMathelator =addArray(theArrayMath);
+console.log("The total is " + theMathelator);
